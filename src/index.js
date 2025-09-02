f845243888c3c5fea8e7ec71b270c037d8c7e48f
Swapnil Charjan <swapnilcharjan00@gmail.com>
2025-09-02 15:33:07 +0530

    Utils code discusion

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-// require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/db.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`\n Server is running at http://localhost:${process.env.PORT || 8000}`);            
+        })
+    })
+    .catch((error) => {
+        console.log("MONGO DB connection failed !!!", error)
+        throw error
+    })
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+// Reusable promise
+const asyncHandler = (requestHandler) => (req, res, next) => {
+   Promise.resolve(requestHandler(req, res, next))
+   .catch((error) => next(error))
+}
+
+export {asyncHandler};
+
+// Reusable trycatch
+// const asyncHandler = (func) => async (req, res, next) => {
+//     try {
+//         await func(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
+// export {asyncHandler};

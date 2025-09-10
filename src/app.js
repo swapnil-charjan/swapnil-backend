db194d3b1cd682fc5760b6ae230e30f0569eca76
Swapnil Charjan <swapnilcharjan00@gmail.com>
2025-09-10 18:48:36 +0530

    Controller added

--- src/app.js
+++ src/app.js
@@ -10,10 +10,17 @@ app.use(cors({
 }))
 
 
-app.use(express.json({ imit: "16kb" }))
+app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
-app.use(express.cookieParser())
+app.use(cookieParser())
 
+
+//routes import
+import userRouter from "./routes/user.routes.js"
+
+
+//routes declaration 
+app.use("/api/v1/users", userRouter)
 
 export { app }
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,12 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const registerUser = asyncHandler(async (req, res) => {
+    console.log("Hello")
+    res.status(200).json({
+        message: "Ok!"
+    })
+})
+
+export {
+    registerUser,
+}

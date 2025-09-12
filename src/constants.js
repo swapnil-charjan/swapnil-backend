11f7b0c208f00eb0ebb6cbbffbf7ae6b5fb9d290
Swapnil Charjan <swapnilcharjan00@gmail.com>
2025-09-12 13:15:49 +0530

    add register controller and bugs fixes

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 export const DB_NAME = "backend";
+export const PREFIX = "/api/v1/users";  //Use for URL pre-fix
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -20,3 +20,6 @@ class ApiError extends Error {
     }
 }
  
+export {
+    ApiError
+}
